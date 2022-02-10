6304d61b488b99dc73e55e68db06358f7add448c
charichu <32455463+charichu@users.noreply.github.com>
2022-02-11 00:58:18 +0100

    removed errors and added tests

--- go.mod
+++ go.mod
@@ -2,4 +2,14 @@ module github.com/charichu/oauthgo
 
 go 1.17
 
-require github.com/federicoleon/golang-restclient v0.0.0-20191104170228-162ed620df66
+require (
+	github.com/charichu/utilsgo v0.0.0-20220210233927-d48e0cdb10db
+	github.com/federicoleon/golang-restclient v0.0.0-20191104170228-162ed620df66
+	github.com/stretchr/testify v1.7.0
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
+)
--- oauth/oauth.go
+++ oauth/oauth.go
@@ -2,8 +2,9 @@ package oauth
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
-	"github.com/charichu/oauthgo/errors"
+	"github.com/charichu/utilsgo/rest_errors"
 	"github.com/federicoleon/golang-restclient/rest"
 	"net/http"
 	"strconv"
@@ -60,7 +61,7 @@ func GetUserId(request *http.Request) int64 {
 	return clientId
 }
 
-func AuthenticateRequest(request *http.Request) *errors.RestErr {
+func AuthenticateRequest(request *http.Request) rest_errors.RestErr {
 	if request == nil {
 		return nil
 	}
@@ -74,7 +75,7 @@ func AuthenticateRequest(request *http.Request) *errors.RestErr {
 
 	at, err := getAccessToken(accessTokenId)
 	if err != nil {
-		if err.Code == http.StatusNotFound {
+		if err.Status() == http.StatusNotFound {
 			return nil
 		}
 		return err
@@ -94,23 +95,24 @@ func cleanRequest(request *http.Request) {
 	request.Header.Del(headerXCallerId)
 }
 
-func getAccessToken(accessTokenId string) (*accessToken, *errors.RestErr) {
+func getAccessToken(accessTokenId string) (*accessToken, rest_errors.RestErr) {
 	response := oauthRestClient.Get(fmt.Sprintf("/oauth/access_token/%s", accessTokenId))
-
 	if response == nil || response.Response == nil {
-		return nil, errors.NewInternalServerError("invalid rest client response when trying to get access token")
+		return nil, rest_errors.NewInternalServerError("invalid rest client response when trying to get access token",
+			errors.New("network timeout"))
 	}
+
 	if response.StatusCode > 299 {
-		var restErr errors.RestErr
+		var restErr rest_errors.RestErr
 		err := json.Unmarshal(response.Bytes(), &restErr)
 		if err != nil {
-			return nil, errors.NewInternalServerError("error with invalid interface when trying to get access token")
+			return nil, rest_errors.NewInternalServerError("error with invalid interface when trying to get access token", err)
 		}
-		return nil, &restErr
+		return nil, restErr
 	}
 	var at accessToken
 	if err := json.Unmarshal(response.Bytes(), &at); err != nil {
-		return nil, errors.NewInternalServerError("error when trying to unmarshal access token response")
+		return nil, rest_errors.NewInternalServerError("error when trying to unmarshal access token response", errors.New("error processing json"))
 	}
 	return &at, nil
 }
